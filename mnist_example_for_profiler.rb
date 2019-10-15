9c85eae244e5022ea04fcd61970d7483b8fb1c7c
kojix2 <2xijok@gmail.com>
2019-10-15 23:20:38 +0900

    Use CNN

--- mnist_example_for_profiler.rb
+++ mnist_example_for_profiler.rb
@@ -14,12 +14,28 @@ y_train = SFloat.cast Marshal.load(File.binread('y_train.dat'))
 y_test  = SFloat.cast Marshal.load(File.binread('y_test.dat'))
 
 model = Sequential.new
-model << InputLayer.new(784)
-model << Dense.new(256)
-model << ReLU.new
-model << Dense.new(256)
-model << ReLU.new
-model << Dense.new(10)
-model.setup(RMSProp.new, SoftmaxCrossEntropy.new)
 
-model.train(x_train, y_train, 20, batch_size: 100, test: [x_test, y_test], verbose: true)
+model << InputLayer.new([28, 28, 1])
+
+model << Conv2D.new(16, 5)
+model << BatchNormalization.new
+model << ReLU.new
+
+model << MaxPool2D.new(2)
+
+model << Conv2D.new(32, 5)
+model << BatchNormalization.new
+model << ReLU.new
+
+model << Flatten.new
+
+model << Dense.new(256)
+model << BatchNormalization.new
+model << ReLU.new
+model << Dropout.new(0.5)
+
+model << Dense.new(10)
+
+model.setup(Adam.new, SoftmaxCrossEntropy.new)
+
+model.train(x_train, y_train, 10, batch_size: 100, test: [x_test, y_test])
